d91e17f01aa128ca83e27ee47f0c38be32564652
rnjsehdgur <136230839+rnjsehdgur@users.noreply.github.com>
2023-06-13 15:33:12 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,30 +1,35 @@
+import streamlit as st
 import numpy as np
 from scipy import signal
 import matplotlib.pyplot as plt
-import streamlit as st 
- 
-num = [100]
 
-den = [2, 3, 0]
+# 전달함수 계수 입력
+numerator = st.text_input("Numerator coefficients (separated by commas)", "100")
+denominator = st.text_input("Denominator coefficients (separated by commas)", "1, 5, 6")
 
-fig= signal.TransferFunction(num, den)
+# 전달함수 계수 파싱
+num = [float(coeff.strip()) for coeff in numerator.split(",")]
+den = [float(coeff.strip()) for coeff in denominator.split(",")]
 
-t, y = signal.step(fig)
+# 폐루프 전달함수 계산
+L = signal.TransferFunction(num, den)
 
-w, mag, phase = signal.bode(fig)
+# Unit step 입력의 응답곡선 계산
+t, y = signal.step(L)
 
-plt.figure(figsize=(10, 4))
-plt.subplot(1, 2, 1)
-plt.plot(t, y)
-plt.xlabel('Time')
-plt.ylabel('Output')
-plt.title('Step Response')
+# 주파수 응답의 보드선도 계산
+w, mag, phase = signal.bode(L)
 
-plt.subplot(1, 2, 2)
-plt.semilogx(w, mag)
-plt.xlabel('Frequency [rad/s]')
-plt.ylabel('Magnitude [dB]')
-plt.title('Bode Diagram')
+# 그래프 그리기
+fig, axs = plt.subplots(1, 2, figsize=(12, 4))
 
-plt.tight_layout()
+# 응답곡선 그리기
+axs[0].plot(t, y)
+axs[0].set(xlabel='Time', ylabel='Output', title='Step Response')
+
+# 보드선도 그리기
+axs[1].semilogx(w, mag)
+axs[1].set(xlabel='Frequency [rad/s]', ylabel='Magnitude [dB]', title='Bode Diagram')
+
+# 그래프 출력
 st.pyplot(fig)
