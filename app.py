da8d4f3ee7fc7d1928f9685bbad52557f5b03dcd
rnjsehdgur <136230839+rnjsehdgur@users.noreply.github.com>
2023-06-13 15:21:37 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,21 +3,16 @@ from scipy import signal
 import matplotlib.pyplot as plt
 import streamlit as st 
  
-#전달함수 계수
 num = [100]
 
 den = [2, 3, 0]
 
-#폐루프 전달함수 계산
 L = signal.TransferFunction(num, den)
 
-#Unit step 입력의 응답곡선 계산
 t, y = signal.step(L)
 
-#주파수 응답의 보드선도 계산
 w, mag, phase = signal.bode(L)
 
-#응답곡선 그리기
 plt.figure(figsize=(10, 4))
 plt.subplot(1, 2, 1)
 plt.plot(t, y)
@@ -25,7 +20,6 @@ plt.xlabel('Time')
 plt.ylabel('Output')
 plt.title('Step Response')
 
-#보드선도 그리기
 plt.subplot(1, 2, 2)
 plt.semilogx(w, mag)
 plt.xlabel('Frequency [rad/s]')
